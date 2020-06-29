9dafb060e65174d190bc9e57b38a725d78227cfd
Kadn <kartushin01@gmail.com>
2020-06-30 00:17:49 +0300

    Parts 1-3

--- src/Otus.Teaching.PromoCodeFactory.WebHost/Startup.cs
+++ src/Otus.Teaching.PromoCodeFactory.WebHost/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Otus.Teaching.PromoCodeFactory.Core.Abstractions.Repositories;
@@ -18,20 +19,31 @@ namespace Otus.Teaching.PromoCodeFactory.WebHost
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+         
+            services.AddDbContext<DataContext>(x =>
+            {
+                //x.UseSqlite("Filename=PromoCodeFactoryDb.sqlite");
+                x.UseNpgsql(Configuration.GetConnectionString("DefaultConnection"));
+                //x.UseLazyLoadingProxies();
+            });
+
             services.AddControllers().AddMvcOptions(x=> 
                 x.SuppressAsyncSuffixInActionNames = false);
             services.AddScoped(typeof(IRepository<>), typeof(EfRepository<>));
+            
             services.AddScoped<IDbInitializer, EfDbInitializer>();
-            services.AddDbContext<DataContext>(x =>
-            {
-                x.UseSqlite("Filename=PromoCodeFactoryDb.sqlite");
-                x.UseLazyLoadingProxies();
-            });
-
+            
             services.AddOpenApiDocument(options =>
             {
                 options.Title = "PromoCode Factory API Doc";
