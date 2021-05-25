432f363a9eab72a7caf8153ba77ddf82953b86a0
Stanislav Ageev <progpow@gmail.com>
2021-05-25 16:33:48 +0300

    Add migration name as docker container parameter

---
+++ src/Otus.Teaching.PromoCodeFactory.DataAccess/Migrations/20210524124108_AddPatronymic.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Otus.Teaching.PromoCodeFactory.DataAccess.Migrations
+{
+    public partial class AddPatronymic : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Patronymic",
+                table: "Customers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Patronymic",
+                table: "Customers");
+        }
+    }
+}
